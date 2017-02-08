c4289b6248f2dcbb523a595a0a006e1741216ed5
DimaStanishevskiy <stanishevskiy.dmitro@gmail.com>
2017-02-08 22:57:54 +0200

    Add visual version

---
+++ OOP2-Visual/ElectricityConsumer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace OOP2_Visual
+{
+    public class ElectricityConsumer
+    {
+        public readonly string nameConsumers;
+        public readonly float consumptionOnPlan;
+        public readonly float consumptionInFact;
+        public readonly float mistake;
+        public readonly float pMistake;
+
+        public ElectricityConsumer(string name, float OnPlan, float InFact)
+        {
+            nameConsumers = name;
+            consumptionOnPlan = OnPlan;
+            consumptionInFact = InFact;
+            mistake = Math.Abs(consumptionOnPlan - consumptionInFact);
+            pMistake = (mistake * 100) / consumptionOnPlan;
+        }
+    }
+    class ConsumersContext : DbContext
+    {
+        public ConsumersContext() : base("ElectricityConsumerConnectionString")
+        { }
+
+        public DbSet<ElectricityConsumer> ElectricityConsumer { get; set; }
+    }
+}
---
+++ OOP2-Visual/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace OOP2_Visual
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.electricityConsumerDataSet = new OOP2_Visual.ElectricityConsumerDataSet();
+            this.electricityConsumerDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.electricityConsumerDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.electricityConsumerDataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.DataSource = this.electricityConsumerDataSetBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(5, 7);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(267, 172);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // electricityConsumerDataSet
+            // 
+            this.electricityConsumerDataSet.DataSetName = "ElectricityConsumerDataSet";
+            this.electricityConsumerDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // electricityConsumerDataSetBindingSource
+            // 
+            this.electricityConsumerDataSetBindingSource.DataSource = this.electricityConsumerDataSet;
+            this.electricityConsumerDataSetBindingSource.Position = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.electricityConsumerDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.electricityConsumerDataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource electricityConsumerDataSetBindingSource;
+        private ElectricityConsumerDataSet electricityConsumerDataSet;
+    }
+}
+
